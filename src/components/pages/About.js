992648f74e1f3588714725a30dec70373684af6e
uiido <uiido@yahoo.com>
2023-02-07 02:16:52 -0500

    Minor changes to about for brainstorming

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div class="container p-3 mt-3">
+        <div class="container p-3 mt-3 text-center">
             <h2 class="text-center pb-3">Here's what to know about me</h2>
+
+            <img src="https://via.placeholder.com/150" />
+
+            <figure class="text-center">
+                <blockquote class="blockquote">
+                    <p class="mb-0">"If you aim at nothing, you hit nothing."</p>
+
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                    <cite title="Source Title">Shang-Chi and the Legend of the Ten Rings</cite>
+                </figcaption>
+            </figure>
+
             <p>Coming soon ...</p>
 
         </div>
