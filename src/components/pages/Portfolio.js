920c5ecffc3b675728844e719c7acc2f3f976854
uiido <uiido@yahoo.com>
2023-02-06 23:13:22 -0500

    Fixed portfolio image respnosivity, but now columns and rows are incorrect

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,37 +6,37 @@ export default function Portfolio() {
             <h2 class="text-center pb-3">View My Work</h2>
             <div class="row">
                 <div class="col">
-                    <img class="rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="400" height="140" />
+                    <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid rounded' alt='...' />
                     <h2>Heading</h2>
                     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View Code »</a> <a class="btn btn-primary" href="#" role="button">View Site »</a></p>
                 </div>
                 <div class="col">
-                    <img class="rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="400" height="140" />
+                    <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid rounded' alt='...' />
                     <h2>Heading</h2>
                     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View Code »</a> <a class="btn btn-primary" href="#" role="button">View Site »</a></p>
                 </div>
                 <div class="col">
-                    <img class="rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="400" height="140" />
+                    <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid rounded' alt='...' />
                     <h2>Heading</h2>
                     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View Code »</a> <a class="btn btn-primary" href="#" role="button">View Site »</a></p>
                 </div>
                 <div class="col">
-                    <img class="rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="400" height="140" />
+                    <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid rounded' alt='...' />
                     <h2>Heading</h2>
                     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View Code »</a> <a class="btn btn-primary" href="#" role="button">View Site »</a></p>
                 </div>
                 <div class="col">
-                    <img class="rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="400" height="140" />
+                    <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid rounded' alt='...' />
                     <h2>Heading</h2>
                     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View Code »</a> <a class="btn btn-primary" href="#" role="button">View Site »</a></p>
                 </div>
                 <div class="col">
-                    <img class="rounded" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="400" height="140" />
+                    <img src='https://mdbootstrap.com/img/new/slides/041.webp' className='img-fluid rounded' alt='...' />
                     <h2>Heading</h2>
                     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View Code »</a> <a class="btn btn-primary" href="#" role="button">View Site »</a></p>
